5eacd79fe3aa34dd53a0509da7a9b2b4b6e79e5b
byron <brionariomen@gmail.com>
2025-03-29 08:39:49 -0500

    prueba interna 2025-03-29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.elrancho.cocina"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
+        versionCode = 8
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
--- app/src/main/java/com/elrancho/cocina/MainActivity.kt
+++ app/src/main/java/com/elrancho/cocina/MainActivity.kt
@@ -1,28 +1,26 @@
 package com.elrancho.cocina
 
-import android.content.ClipData.Item
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import okhttp3.*
 import org.json.JSONObject
 import java.io.IOException
-
-import android.widget.Button
-import androidx.recyclerview.widget.LinearLayoutManager
-
-
 import com.elrancho.cocina.consultaMysql.consulta
 import com.elrancho.cocina.menuCarta.CartaMenu
+import com.elrancho.cocina.usuarios.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var usuario: EditText
     private lateinit var producto: EditText
     private val client = OkHttpClient()
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,23 +28,37 @@ class MainActivity : AppCompatActivity() {
         usuario = findViewById(R.id.usuario)
         producto = findViewById(R.id.producto)
 
-
-
-
-        ///con este boton vamos a la actividad consulta
+        // Recupera el nombre del usuario desde SharedPreferences
+        val sharedPreferences = getSharedPreferences("user_session", Context.MODE_PRIVATE)
+        val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false)
+/*
+        if (isLoggedIn) {
+            val username = sharedPreferences.getString("username", "Usuario")
+            findViewById<TextView>(R.id.usernameTextView).text = "Bienvenido, $username"
+        } else {
+            // Si no ha iniciado sesión, redirige a LoginActivity
+            startActivity(Intent(this, LoginActivity::class.java))
+           // finish()
+        }
+*/
+        // Botón para ir a la actividad consulta
         val buttonNavigate2: Button = findViewById(R.id.consulta)
         buttonNavigate2.setOnClickListener {
-            // Create an Intent to start SecondActivity
             val intent = Intent(this, consulta::class.java)
             startActivity(intent)
         }
 
-
-///con este boton vamos a la actividad carta
+        // Botón para ir a la actividad carta
         val buttonNavigate3: Button = findViewById(R.id.carta)
         buttonNavigate3.setOnClickListener {
-            // Create an Intent to start SecondActivity
-            val intent = Intent(this,CartaMenu::class.java)
+            val intent = Intent(this, CartaMenu::class.java)
+            startActivity(intent)
+        }
+
+        // Botón para ir a la actividad de login
+        val buttonNavigate1: Button = findViewById(R.id.iralogin)
+        buttonNavigate1.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
@@ -65,10 +77,6 @@ class MainActivity : AppCompatActivity() {
             .url(url)
             .post(formBody)
             .build()
-///fwewfwefwedfewfdewfwefewfew
-
-        ///mañana tendre mucho dinero
-
 
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
@@ -76,6 +84,7 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(applicationContext, "Error: ${e.message}", Toast.LENGTH_LONG).show()
                 }
             }
+
             override fun onResponse(call: Call, response: Response) {
                 response.body?.let { responseBody ->
                     val responseData = responseBody.string()
@@ -87,9 +96,6 @@ class MainActivity : AppCompatActivity() {
                             "Error: ${json.getString("message")}"
                         }
                         Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
-
-
-                        //josjadjaoj
                     }
                 }
             }
